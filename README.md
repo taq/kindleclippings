b4c194e5bf4703a8fcee34a31479286e9937896d
Eustaquio Rangel <taq@eustaquiorangel.com>
2022-07-05 09:17:54 -0300

    Ruby script

---
+++ README.md
@@ -0,0 +1,21 @@
+# Scripts to extract Kindle clippings
+
+All scripts need the clippings file (usually named 'My Clippings.txt') and a
+expression to search, for example, the book name.
+
+## Shell script
+
+Use the `kindleclipping.sh` script:
+
+```
+$ ./kindleclipping.sh <clippings file> <expression>
+```
+
+## Ruby script
+
+This is an updated version of the shell script.
+Use the `kindleclipping.rb` script:
+
+```
+$ ruby kindleclipping.rb <clippings file> <expression>
+```
---
+++ kindleclippings.rb
@@ -0,0 +1,96 @@
+# typed: false
+# frozen_string_literal: true
+
+class Book
+  attr_accessor :title, :author, :notes
+
+  def initialize(title, author)
+    @title  = title
+    @author = author
+    @notes  = {}
+  end
+
+  def print
+    puts "#{@title} - #{@author}"
+    puts '-' * 80
+    puts "\n"
+
+    for pos, note in @notes
+      sep = "-" * pos.size
+      puts "#{pos}\n#{sep}\n#{note.text.join("\n")}\n\n"
+    end
+  end
+end
+
+class Note
+  attr_accessor :position, :text
+
+  def initialize(position, text)
+    @position = position
+    @text     = text
+  end
+end
+
+file   = ARGV[0].to_s
+filter = ARGV[1].to_s
+
+if file.size < 1
+  STDERR.puts "Error: need a file name"
+  return
+end
+
+unless File.exist?(file)
+  STDERR.puts "Error: file #{file} does not exist"
+  return
+end
+
+title = true
+date  = false
+books = {}
+bref  = nil
+pref  = nil
+
+title_exp = /^(?<title>.*) \((?<author>.*)\)/
+pos_exp   = /(?<pos>\d+(-\d+)?)/
+
+File.open(file).each do |line|
+  line = line.chomp
+
+  if title
+    matches = line.match(title_exp)
+    title   = false
+    date    = true
+    bref    = line
+    books[bref] ||= Book.new(matches[:title], matches[:author])
+    next
+  end
+
+  if date
+    matches = line.match(pos_exp)
+    pref    = matches[:pos]
+    date    = false
+    books[bref].notes[pref] ||= Note.new(pref, [])
+    next
+  end
+
+  if line == '=========='
+    bref  = nil
+    pref  = nil
+    title = true
+    date  = false
+    next
+  end
+
+  next if line.size < 1
+
+  books[bref].notes[pref].text << line
+end
+
+if filter.size > 0
+  filter_exp = Regexp.new(filter, true)
+  books = books.select { |key, book| book.title.match?(filter_exp) }
+end
+
+for key, book in books
+  book.print
+end
